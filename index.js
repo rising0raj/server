c7338c9b710437df59bb3be0f767534bff9194df
rising0raj <risingraj26@gmail.com>
2023-04-18 02:07:12 +0530

    new code

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const PORT=process.env.PORT || 5000
 
 const DATABASE_URL=process.env.CONNECTION_URL
 
+mongoose.set("strictQuery", true);
 mongoose.connect(DATABASE_URL, {useNewUrlParser:true,useUnifiedTopology:true} )
     .then(()=>app.listen(PORT, () =>{console.log(`Server running on PORT ${PORT}`)}))
     .catch((err)=>console.log(err.message))
