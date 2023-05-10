a112ba413dc3265f43b515a9b78e642728f25f96
rising0raj <risingraj26@gmail.com>
2023-05-10 16:51:17 +0530

    5656

--- controllers/paymentcontroller.js
+++ controllers/paymentcontroller.js
@@ -29,7 +29,7 @@ export const paymentVerify = async(req,res)=>{
 
     const isAuthpay= signature === razorpay_signature;
     if(isAuthpay){
-        res.redirect(`https://stackoverflow-clone-by-httprising.netlify.app/paymentsuccess?reference=${razorpay_payment_id}`)
+        res.redirect("https://stackoverflow-clone-by-httprising.netlify.app")
     }
     else{
         res.status(200).json({sucess:false})
